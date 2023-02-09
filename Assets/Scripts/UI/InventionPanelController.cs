726733e27428344219689afb62afd62e44cbd551
redseiko <redseiko@users.noreply.github.com>
2023-02-08 20:04:04 -0800

    Create InventionPanelController.
    Finish the DurabilityBar look to have left/right ends.

---
+++ Assets/Scripts/UI/InventionPanelController.cs
@@ -0,0 +1,21 @@
+﻿using UI.ThreeDimensional;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventionPanelController : MonoBehaviour {
+  [field: SerializeField]
+  public GameObject InventionPanel { get; private set; }
+
+  [field: SerializeField]
+  public TMPro.TMP_Text InventionName { get; private set; }
+
+  [field: SerializeField]
+  public UIObject3D InventionRender { get; private set; }
+
+  [field: SerializeField]
+  public TMPro.TMP_Text DurabilityText { get; private set; }
+
+  [field: SerializeField]
+  public Slider DurabilityBar { get; private set; }
+}
